9cc6c82b45d38aedbe4c2f7db8f6d365c92bf479
dima <dimags500@gmail.com>
2022-11-16 13:16:15 +0200

    Init commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int userId)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            return user;
+        }
+    }
+}
